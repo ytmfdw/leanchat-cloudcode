8895a77ffae2d3216df87a63c8e97a57f15ffaf1
lzwjava <lzwjava@gmail.com>
2015-04-29 14:09:23 +0800

    add hook

--- cloud/main.js
+++ cloud/main.js
@@ -7,3 +7,6 @@ AV.Cloud.define("conv_sign", msign.convSign);
 
 AV.Cloud.define("_messageReceived",mchat.messageReceived);
 AV.Cloud.define("_receiversOffline", mchat.receiversOffline);
+AV.Cloud.define("_conversationAdd",mchat.conversationAdd);
+AV.Cloud.define("_conversationRemove",mchat.conversationRemove);
+AV.Cloud.define("_conversationStart",mchat.conversationStart);
